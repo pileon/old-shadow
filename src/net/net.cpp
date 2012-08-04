74300da54e4232abe1a913dc57fc5090518c7a8c
Joachim Pileborg <arrow@pileborg.se>
2012-08-04 20:07:26 +0200

    Make sure telnet is enabled

--- src/net/net.cpp
+++ src/net/net.cpp
@@ -132,10 +132,14 @@ bool init()
 	// TODO: Create the listening socket(s)
 	create_server();
 
-	server_list_.push_back(
-		server_ptr_t{
-			new server::TCP{io_service_,
-			config::get<unsigned short>("net.telnet.port")}});
+	if (config::exists("net.telnet.enabled") &&
+		config::get<bool>("net.telnet.enabled"))
+	{
+		server_list_.push_back(
+			server_ptr_t{
+				new server::TCP{io_service_,
+				config::get<unsigned short>("net.telnet.port")}});
+	}
 
 	// To not block the main thread we have to create a new thread
 	// that will handle the ASIO event loop
